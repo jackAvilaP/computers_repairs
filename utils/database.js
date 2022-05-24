1263be7d618ab3ee006a406e1599b0868fc18a25
jackAvilaP <arenca44@gmail.com>
2022-05-24 02:57:48 -0500

    cambios a postgres

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ dotenv.config({ path: './config.env' });
 
 // Create a connection to database
 const db = new Sequelize({
-  dialect: 'mysql',
+  dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
